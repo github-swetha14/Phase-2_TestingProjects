cdbe4f7a752f40577c64dce0d114f8f741166745
github-swetha14 <148947627+github-swetha14@users.noreply.github.com>
2023-12-02 08:28:27 +0000

    Add files via upload

---
+++ CucumberPhase2EndProject/test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"d9e0779e-8eaf-4a85-a2fa-cd22e07728a9","feature":"Test the starHealth page on Chrome Browser","scenario":"Validate the Star Health Buy Now flow","start":1701498394747,"group":1,"content":"","tags":"","end":1701498461457,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
